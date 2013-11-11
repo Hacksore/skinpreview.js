11507e21e2a1b7e8da879ce29b38f01f591c8604
Hacksore <hacksore@mcskinsearch.com>
2013-11-11 10:20:21 -0800

    updated unit tests, removed search since it used an experimental api

--- skinpreview.js
+++ skinpreview.js
@@ -2,171 +2,198 @@
 	A canvas Minecraft skin renderer
 	Project link: https://github.com/Hacksore/skinpreview.js
 */
-function SkinPreview(options) {
-	var self = this;
-	var s = 1;
-	var skinImage = new Image();
-	var skinLoaded = false;
-	var capeloaded = false;
-	var capeEnabled = false;
-	var errorImage = new Image();	
-	var canvas = document.createElement('canvas');
-	var context = canvas.getContext("2d");
-	var _can = document.createElement('canvas');
-	var _ctx = _can.getContext("2d");
 
-	canvas.id = "skin-preview";	
-	_can.width = 64;
-	_can.height = 32;
-	
+(function ($) {
+    'use strict';
 
-	this.user = "";		
-	this.cape = new Image();
-
-	this.drawSkinFront = function(){	
-		var parts = {
-			head: [skinImage, 8, 8, 8, 8, 4, 0, 8, 8],
-			body: [skinImage, 20, 20, 8, 12, 4, 8, 8, 12],
-			leftleg: [skinImage, 4, 20, 4, 12, 4, 20, 4, 12],
-			leftarm: [skinImage, 44, 20, 4, 12, 0, 8, 4, 12],
-			rightleg: [_can, 56, 20, 4, 12, 8, 20, 4, 12],
-			rightarm: [_can, 16, 20, 4, 12, 12, 8, 4, 12]
-		};		
-		this.drawSkin(parts);
-	}	
-	
-	
-	this.drawSkinBack = function(){		
-		var parts = {
-			head: [skinImage, 24, 8, 8, 8, 4, 0, 8, 8],
-			body: [skinImage, 32, 20, 8, 12, 4, 8, 8, 12],			
-			rightarm: [skinImage, 52, 20, 4, 12, 12, 8, 4, 12],
-			rightleg: [skinImage, 12, 20, 4, 12, 8, 20, 4, 12],	
-			leftleg: [_can, 48, 20, 4, 12, 4, 20, 4, 12],
-			leftarm: [_can, 8, 20, 4, 12, 0, 8, 4, 12]
-			
-		};	
-		this.drawSkin(parts);
-	}
-	
-	this.drawSkin = function(parts){
-		if(!skinLoaded) { return; }		
-		context.imageSmoothingEnabled = false;
-		context.mozImageSmoothingEnabled = false;
-		context.oImageSmoothingEnabled = false;
-		context.webkitImageSmoothingEnabled = false;
-		context.clearRect(0, 0, canvas.width, canvas.height);
-
-		//draw parts
-		for(var i in parts){		
-			var v = parts[i];	
-			context.drawImage(v[0], v[1], v[2], v[3], v[4], v[5]*s, v[6]*s, v[7]*s, v[8]*s);			
-		}
-	}
+	function SkinPreview(options) {
+		var self = this;
+		var s = 1;
+		var skinImage = new Image();
+		var skinLoaded = false;
+		var capeloaded = false;
+		var capeEnabled = false;
+		var errorImage = new Image();	
+		var canvas = document.createElement('canvas');
+		var context = canvas.getContext("2d");
+		var _can = document.createElement('canvas');
+		var _ctx = _can.getContext("2d");
+		var options = {};
+		_can.width = 64;
+		_can.height = 32;
 		
-	this.setScale = function(scale){		
-		s = scale;
-		canvas.width = 16 * s;
-		canvas.height = 32 * s;
-		this.drawSkinFront();
-	}
-	
-	this.setSkin = function(user){	
-		capeloaded = false;
-		skinImage = new Image();
-		skinImage.src = "https://s3.amazonaws.com/MinecraftSkins/" + user+ ".png";
-		skinImage.onload = function(){
-			skinLoaded = true;
-			_ctx.scale(-1, 1);
-			_ctx.drawImage(skinImage, -64, 0);
-			_ctx.scale(-1, 1);
-			self.drawSkinFront();
-	
-		}
-		skinImage.onerror = function(){
-			skinLoaded = true;
-			skinImage = errorImage;
-			_ctx.scale(-1, 1);
-			_ctx.drawImage(skinImage, -64, 0);
-			_ctx.scale(-1, 1);
-			self.drawSkinFront();
-		}
-		this.getCape(user);
-	}	
 
-	this.getCape = function(user){
-		this.cape.src = "https://s3.amazonaws.com/MinecraftCloaks/" + user + ".png";
-		this.cape.onload = function(){
-			capeloaded = true;
+		this.user = "";		
+		this.cape = new Image();
+		this.drawHead = function(){
+			context.imageSmoothingEnabled = false;
+			context.mozImageSmoothingEnabled = false;
+			context.oImageSmoothingEnabled = false;
+			context.webkitImageSmoothingEnabled = false;
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			context.drawImage(skinImage, 8, 8, 8, 8, 0, 0, s*8, s*8);		
 			
 		}
-		this.cape.onerror = function(){
-			capeloaded = false;		
+		this.drawSkinFront = function(){	
+			var parts = {
+				head: [skinImage, 8, 8, 8, 8, 4, 0, 8, 8],
+				body: [skinImage, 20, 20, 8, 12, 4, 8, 8, 12],
+				leftleg: [skinImage, 4, 20, 4, 12, 4, 20, 4, 12],
+				leftarm: [skinImage, 44, 20, 4, 12, 0, 8, 4, 12],
+				rightleg: [_can, 56, 20, 4, 12, 8, 20, 4, 12],
+				rightarm: [_can, 16, 20, 4, 12, 12, 8, 4, 12]
+			};		
+			this.drawSkin(parts);
+		}	
+		
+		
+		this.drawSkinBack = function(){		
+			var parts = {
+				head: [skinImage, 24, 8, 8, 8, 4, 0, 8, 8],
+				body: [skinImage, 32, 20, 8, 12, 4, 8, 8, 12],			
+				rightarm: [skinImage, 52, 20, 4, 12, 12, 8, 4, 12],
+				rightleg: [skinImage, 12, 20, 4, 12, 8, 20, 4, 12],	
+				leftleg: [_can, 48, 20, 4, 12, 4, 20, 4, 12],
+				leftarm: [_can, 8, 20, 4, 12, 0, 8, 4, 12]
+				
+			};	
+			this.drawSkin(parts);
 		}
-	}
-	
-	this.drawCape = function(){
-		if(!capeEnabled){ return; }
-		if(!capeloaded){ return; }
-		if(this.cape == null){ return; }
+		
+		this.drawSkin = function(parts){
+			if(!skinLoaded) { return; }		
+			context.imageSmoothingEnabled = false;
+			context.mozImageSmoothingEnabled = false;
+			context.oImageSmoothingEnabled = false;
+			context.webkitImageSmoothingEnabled = false;
+			context.clearRect(0, 0, canvas.width, canvas.height);
 
-		var w = 12;
-		var h = 17;
-		context.drawImage(this.cape, 0, 0, w, h, s * 2, s * 8, s * w, s * h);
-	}
-	
-	canvas.onmouseover = function(){
-		self.drawSkinBack()
-		self.drawCape();
-	}
-
-	canvas.onmouseleave = function(){
-		self.drawSkinFront();
-	}	
-	
-	canvas.oncontextmenu = function(e){	
-		e.preventDefault();		
-		if(!capeloaded){ return; }
-		capeEnabled = !capeEnabled;
-		self.drawSkinBack();
-		self.drawCape();	
-	}
-
-	this.init = function(){	
-		var _opts = {
-			scale: 20,
-			skin: "Hacksore",
-			cape: false,
-			div: "test",
-			className: "",
-			prefix: "<div>",
-			suffix: "</div>",
-			default_skin: "char.png"
-			
-		};
-		for(var i in _opts){
-			if(options[i] == null){
-				options[i] = _opts[i];
+			//draw parts
+			for(var i in parts){		
+				var v = parts[i];	
+				context.drawImage(v[0], v[1], v[2], v[3], v[4], v[5]*s, v[6]*s, v[7]*s, v[8]*s);			
 			}
 		}
-
-		capeEnabled = options.cape;
-
-		errorImage.src = options.default_skin;
-		errorImage.onload = function() {
-		}			
-		
-		this.setScale(options.scale);
-		this.setSkin(options.skin);
-		this.user = options.skin;		
 			
-		canvas.className = options.className		
+		this.setScale = function(scale){		
+			s = scale;
+			canvas.width = 16 * s;
+			canvas.height = 32 * s;
+			this.drawSkinFront();
+		}
 		
-		options.prefix = options.prefix.replace("{:username}", this.user);		
-		var el = $(options.prefix+options.suffix).html(canvas);
-		$("#"+options.div).append(el);
-	}	
+		this.setSkin = function(user){	
+			capeloaded = false;
+			skinImage = new Image();
+			skinImage.src = "https://s3.amazonaws.com/MinecraftSkins/" + user+ ".png";
+			skinImage.onload = function(){
+				skinLoaded = true;
+				_ctx.scale(-1, 1);
+				_ctx.drawImage(skinImage, -64, 0);
+				_ctx.scale(-1, 1);
 
-	this.init();
+				if(options.head){
+					canvas.width = 8 * s;
+					canvas.height = 8 * s;
+					self.drawHead();
+					return;
+				}	
+				self.drawSkinFront();
+		
+			}
+			skinImage.onerror = function(){
+				skinLoaded = true;
+				skinImage = errorImage;
+				_ctx.scale(-1, 1);
+				_ctx.drawImage(skinImage, -64, 0);
+				_ctx.scale(-1, 1);
+				if(options.head){
+					canvas.width = 8 * s;
+					canvas.height = 8 * s;
+					self.drawHead();
+					return;
+				}	
+				self.drawSkinFront();
+			}
+			this.getCape(user);
+		}	
+
+		this.getCape = function(user){
+			this.cape.src = "https://s3.amazonaws.com/MinecraftCloaks/" + user + ".png";
+			this.cape.onload = function(){
+				capeloaded = true;
+				
+			}
+			this.cape.onerror = function(){
+				capeloaded = false;		
+			}
+		}
+		
+		this.drawCape = function(){
+			if(!capeEnabled){ return; }
+			if(!capeloaded){ return; }
+			if(this.cape == null){ return; }
+
+			var w = 12;
+			var h = 17;
+			context.drawImage(this.cape, 0, 0, w, h, s * 2, s * 8, s * w, s * h);
+		}
+		
+		canvas.onmouseover = function(){
+			if(options.head){ return; }
+			self.drawSkinBack()
+			self.drawCape();
+		}
+
+		canvas.onmouseleave = function(){
+			if(options.head){ return; }
+			self.drawSkinFront();
+		}	
+		
+		canvas.oncontextmenu = function(e){	
+			if(options.head){ return; }
+			if(!options.cape){ return; }
+			e.preventDefault();		
+			if(!capeloaded){ return; }
+			capeEnabled = !capeEnabled;
+			self.drawSkinBack();
+			self.drawCape();	
+		}
+
+		this.init = function(ele, _options){	
+			options = _options;
 	
-}
+			capeEnabled = options.cape;
+
+			errorImage.src = options.default_skin;
+			errorImage.onload = function() {
+			}			
+			
+			this.setScale(options.scale);
+			this.setSkin(options.skin);
+					
+			canvas.className = options.className		
+
+			$(ele).append(canvas);
+		}	
+		
+	}
+
+    $.fn.skinPreview = function (options) {
+		this.each(function () {
+            var defaults = {
+            	scale: 4,
+            	head: false,
+				cape: false,
+				className: "",
+				default_skin: "char.png",
+                skin: $(this).attr('data-player')   
+            };
+     
+            var sp = new SkinPreview();
+            sp.init(this, $.extend(defaults, options));
+        });
+    };
+		
+	
+} (window.jQuery));
